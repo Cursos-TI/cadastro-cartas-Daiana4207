6e8cba140b1c239093ff690897498e8efce8e06a
Daiana4207 <summerted@hotmail.com>
2025-11-30 11:09:22 +0000

    30/11

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,28 +3,29 @@ int main(){
 printf("Desafio super trunfo!\n");
 
 char opcao ;
-char nome [50];
+char nome, codigo [50];
 int populacao ,numero_de_pontos_turisticos;
 float area, pib;
 
 printf("digite o nome da opcao:\n");
-sanf("%s",&opcao);
+scanf("%s",&opcao);
 
-prinf("digite o nome da cidade :\n");
+printf("digite o nome da cidade :\n");
 scanf("%s",&nome );
-printf("digite o cidigo:\n");
-scanf("%s",&codigo)
 
-prinf("digite populacao:\n");
+printf("digite o codigo:\n");
+scanf("%s",&codigo);
 
+printf("digite populacao:\n");
 scanf("%d",&populacao);
-printf(''digite o numero de pontos:\n");
-scanf("%d",&numero de pontos:\n);
+
+printf("digite o numero de pontos turisticos:\n");
+scanf("%d",&numero_de_pontos_turisticos);
 
 printf("digite area:\n");
-sanf("%f",&area);
-prinf("digite o numero do pib:\n");
-scanf("%f,&pib);
+scanf("%f",&area);
+printf("digite o numero do pib:\n");
+scanf("%f",&pib);
 
 
 return 0;
