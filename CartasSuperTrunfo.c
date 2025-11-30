182643c58256f89f5955375d60037dc41c2a3587
Daiana4207 <summerted@hotmail.com>
2025-11-30 10:31:45 +0000

    kkk

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,17 +7,24 @@ char nome [50];
 int populacao ,numero_de_pontos_turisticos;
 float area, pib;
 
-printf("digite o nome da opção:\n");
-sanf("%s",&opção);
+printf("digite o nome da opcao:\n");
+sanf("%s",&opcao);
 
-prinf("digite o nome da cidade e codigo:\n");
+prinf("digite o nome da cidade :\n");
 scanf("%s",&nome );
+printf("digite o cidigo:\n");
+scanf("%s",&codigo)
 
-prinf("digite população e numero de pontos:\n");
+prinf("digite populacao:\n");
+
 scanf("%d",&populacao);
+printf(''digite o numero de pontos:\n");
+scanf("%d",&numero de pontos:\n);
 
 printf("digite area:\n");
-sanf("%f",&area_e_pib);
+sanf("%f",&area);
+prinf("digite o numero do pib:\n");
+scanf("%f,&pib);
 
 
 return 0;
